2ca9c531d6b3b91d0188c97600e7a8d5dd07d8de
setiabudi <budiboy195@gmail.com>
2022-06-23 14:04:15 +0700

    fs

---
+++ asset/style.js
@@ -0,0 +1,23 @@
+let show = true
+console.log(show);
+
+// console.log(show);
+
+const showMobileMenu = () => {
+
+    const mobileMenu = document.getElementById("mobile_menu");
+
+    if (show === true) {
+        mobileMenu.style.transform ="translateY(0)";
+    
+        
+       // mobileMenu.style.opacity = "1";
+        show = false
+
+    } else {
+        mobileMenu.style.transform = "translateY(-350px)";
+        // mobileMenu.style.opacity = "1";
+
+        show = true 
+    }
+}
